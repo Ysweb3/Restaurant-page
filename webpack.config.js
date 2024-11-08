846662e8a2073edca9dbe4fafbe4e529033f882a
Ysweb3 <yssh88972@gmail.com>
2024-11-08 16:07:01 +0530

    Create homepage on template.html

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
@@ -14,4 +15,13 @@ module.exports = {
       template: "./src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      
+    ],
+    },
 };
