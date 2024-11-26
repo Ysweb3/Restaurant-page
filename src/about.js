ca5a4dee3a9de6f79f796df85f72073515e2349d
Ysweb3 <yssh88972@gmail.com>
2024-11-26 11:41:08 +0530

    Add contact page

--- src/about.js
+++ src/about.js
@@ -1,9 +1,23 @@
+import map from './map.jpg';
+
 const renderAbout = () => {
     const contentDiv = document.getElementById("content");
     contentDiv.textContent = "";
     const heading = document.createElement("h1");
-    heading.textContent = "ABOUT";
+    heading.textContent = "Contact";
     contentDiv.appendChild(heading);
+
+    //Text box
+    const textBox = document.createElement("div");
+    textBox.setAttribute("id","map-container");
+    contentDiv.appendChild(textBox);
+    textBox.innerHTML = "Contact - 123456789"
+
+    const mapImg = document.createElement("img");
+    mapImg.className = "map-img"; 
+    mapImg.src = map;
+    textBox.appendChild(mapImg)
+
 
 }
 export default renderAbout;
